5cec55ce11cef40edcf7bf48ee86bd5513b95074
Bobby Flennoy <87404756+BobbyAl@users.noreply.github.com>
2026-03-08 14:04:06 -0500

    feat: update index.css with color, text, and spacing aliases and primitives

--- client/src/pages/StudentSurvey.tsx
+++ client/src/pages/StudentSurvey.tsx
@@ -1,3 +1,3 @@
 export default function StudentSurvey() {
-    return <h1>Student Survey - Coming Soon</h1>;
+    return <h1 className="text-3xl text-brand-primary">Student Survey - Coming Soon</h1>;
 }
